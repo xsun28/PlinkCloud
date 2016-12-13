87acde2a8ce2663645d7a273aac2e9e34730538c
xsun28 <xsun28@emory.edu>
2016-12-13 17:25:16 -0500

    add mapreduce test package

---
+++ src/test/java/edu/emory/plinkcloud/plinkcloud/mapreduce/MaxTemperatureReducer.java
@@ -0,0 +1,24 @@
+package edu.emory.plinkcloud.plinkcloud.mapreduce;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+// vv MaxTemperatureReducerV1
+public class MaxTemperatureReducer
+  extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+  @Override
+  public void reduce(Text key, Iterable<IntWritable> values,
+      Context context)
+      throws IOException, InterruptedException {
+    
+    int maxValue = Integer.MIN_VALUE;
+    for (IntWritable value : values) {
+      maxValue = Math.max(maxValue, value.get());
+    }
+    context.write(key, new IntWritable(maxValue));
+  }
+}
