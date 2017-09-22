0322a236486df70a84840602ad8d3aec73a8860b
cloudera <cloudera@quickstart.cloudera>
2017-09-22 15:22:57 -0700

    deleted unnecessary files such as VCF-Simulator and old configuration files

--- plinkcloud-hbase/src/main/resources/run-hbase.sh
+++ plinkcloud-hbase/src/main/resources/run-hbase.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+
+export HBASE_CONF_DIR=/etc/hbase/conf/
+export HADOOP_CLASSPATH=./plinkcloud-hbase.jar:$HBASE_CONF_DIR:$(hbase classpath):$HADOOP_CLASSPATH
 START=$(date +%s)
 hadoop jar plinkcloud-hbase.jar org.plinkcloud.hbase.HBaseVCF2TPEDSelectedChr -i plinkcloud/input/  -o HBase -r 0.0001 -n $1 -q PASS -c 1-26 -s true -g 9  ## -a
 END=$(date +%s)
